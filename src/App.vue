fa17b04dee69b89a9b7453a2423bef0e4fa901ad
Bram Willem van der Kuip <vdkuipbw@gmail.com>
2026-04-21 16:03:33 +0200

    Adds footer with thanks to artsy

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,25 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="mx-auto max-w-[1280px]">
+  <div class="mx-auto flex min-h-screen max-w-[1280px] flex-col">
     <header class="border-b border-gray-200 px-6 py-4">
       <RouterLink to="/" class="text-xl font-semibold">
         Artsy Browser
       </RouterLink>
     </header>
-    <main class="px-6 py-6">
+    <main class="flex-1 px-6 py-6">
       <RouterView />
     </main>
+    <footer class="border-t border-gray-200 px-6 py-4 text-xs text-gray-500">
+      thanks to:
+      <a
+        href="https://www.artsy.net"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="hover:underline"
+      >
+        artsy
+      </a>
+    </footer>
   </div>
 </template>
- q  
