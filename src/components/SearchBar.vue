a9d6b5e247e6814085cd6c24bcce41f165c7b4b1
Bram Willem van der Kuip <vdkuipbw@gmail.com>
2026-04-21 15:45:59 +0200

    Adds searchbar styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,10 +12,20 @@ watch(input, (value) => emitDebounced(value))
 </script>
 
 <template>
-  <input
-    v-model="input"
-    type="search"
-    placeholder="Search artworks…"
-    class="w-full border border-gray-300 px-3 py-2 text-sm focus:border-gray-500 focus:outline-none"
-  />
+  <div class="relative">
+    <input
+      v-model="input"
+      type="text"
+      placeholder="Search artworks…"
+      class="w-full border border-gray-300 px-3 py-2 pr-14 text-sm focus:border-gray-500 focus:outline-none"
+    />
+    <button
+      v-if="input"
+      type="button"
+      class="absolute top-1/2 right-3 -translate-y-1/2 text-xs text-gray-600 hover:underline"
+      @click="input = ''"
+    >
+      clear
+    </button>
+  </div>
 </template>
